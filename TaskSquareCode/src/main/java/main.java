cef25f6e1004a919323b31c200dd3cb027553600
Vladimir Vejin <vejin.vladimir@gmail.com>
2022-12-04 22:05:12 +0100

    Add code for create square words

--- TaskSquareCode/src/main/java/main.java
+++ TaskSquareCode/src/main/java/main.java
@@ -18,6 +18,25 @@ public class main {
                 System.out.println("Your message have spaces or is empty, enter message again.\n");
         } while (input.length() > 81 || input.length() <= 0 || input.contains(" ") || input.isEmpty());
 
+        String val = input.toLowerCase();
+        List<String> listOfStrings = new ArrayList<String>();
+
+        for (int i = 0; i < val.length(); i += (int) sqrt(val.length())) {
+            String retVal;
+            if (i + (int) sqrt(val.length() + 1) > val.length()) {
+                retVal = val.substring(i);
+                listOfStrings.add(retVal);
+            } else {
+                retVal = val.substring(i, i + (int) sqrt(val.length() + 1));
+                listOfStrings.add(retVal);
+            }
+        }
+        System.out.println("\n======================");
+        for (String s : listOfStrings) {
+            System.out.println(s);
+        }
+        System.out.println("======================");
+
         
     }
 }
