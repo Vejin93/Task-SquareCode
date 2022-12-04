10a94f77b8fdf0afefb7c4d865874052ca2c6ef6
Vladimir Vejin <vejin.vladimir@gmail.com>
2022-12-04 22:03:53 +0100

    Add code for enter message and filter it

--- TaskSquareCode/src/main/java/main.java
+++ TaskSquareCode/src/main/java/main.java
@@ -1,6 +1,23 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+import static java.lang.Math.sqrt;
+
 public class main {
 
-    public static void main(String[] args){
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
 
+        String input;
+
+        do {
+            System.out.println("Enter message(without spaces): ");
+            input = sc.nextLine();
+            if (input.contains(" ") || input.isEmpty())
+                System.out.println("Your message have spaces or is empty, enter message again.\n");
+        } while (input.length() > 81 || input.length() <= 0 || input.contains(" ") || input.isEmpty());
+
+        
     }
 }
